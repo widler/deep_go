42443bac9f49b5c3f4ad29bd8886dc439820ce64
Denis Alekseev <denisalekseev@lamoda.ru>
2026-01-15 00:22:20 +0600

    homework-01

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"math"
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
+func ToLittleEndianGen[T uint | uint16 | uint32 | uint64](number T) T {
+	var res T = 0
+	// не придумал как сделать проверку на максимальное значение типа в дженерике
+	if number == 0 {
+		return number
+	}
+	ptr := unsafe.Pointer(&number)
+	for i := range (int)(unsafe.Sizeof(number)) {
+		res = (res << 8) | (T)(*(*uint8)(unsafe.Add(ptr, i)))
+	}
+	return res
+}
 
 func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+	if number == 0 || number == math.MaxUint32 {
+		return number
+	}
+	var res uint32 = 0
+	ptr := unsafe.Pointer(&number)
+	for i := range (int)(unsafe.Sizeof(number)) {
+		res = (res << 8) | (uint32)(*(*uint8)(unsafe.Add(ptr, i)))
+	}
+	return res // need to implement
 }
 
 func TestСonversion(t *testing.T) {
@@ -45,4 +67,10 @@ func TestСonversion(t *testing.T) {
 			assert.Equal(t, test.result, result)
 		})
 	}
+}
+
+func TestСonversionGen(t *testing.T) {
+	// не придумал как нормально протестировать дженерик
+	assert.Equal(t, (uint16)(0x0102), ToLittleEndianGen((uint16)(0x0201)))
+	assert.Equal(t, (uint32)(0x01020304), ToLittleEndianGen((uint32)(0x04030201)))
 }
