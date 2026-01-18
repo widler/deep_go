9cbb98acf897ddb82e059dc6ed0969856327928d
Denis Alekseev <d.alekseev@2Gis.ru>
2026-01-18 19:37:11 +0600

    homework-2

--- homework/slices_and_arrays/homework_test.go
+++ homework/slices_and_arrays/homework_test.go
@@ -1,5 +1,18 @@
 package main
 
+/*
+// Предположим, что эта очередь будет оперировать только положительными
+// числами (отрицательные числа ей никогда не поступят на вход)
+type CircularQueue struct { ... }
+
+func NewCircularQueue(size int)              // создать очередь с определенным размером буффера
+func (q *CircularQueue) Push(value int) bool // добавить значение в конец очереди (false, если очередь заполнена)
+func (q *CircularQueue) Pop() bool           // удалить значение из начала очереди (false, если очередь пустая)
+func (q *CircularQueue) Front() int          // получить значение из начала очереди (-1, если очередь пустая)
+func (q *CircularQueue) Back() int           // получить значение из конца очереди (-1, если очередь пустая)
+func (q *CircularQueue) Empty() bool         // проверить пустая ли очередь
+func (q *CircularQueue) Full() bool          // проверить заполнена ли очередь
+*/
 import (
 	"reflect"
 	"testing"
@@ -11,35 +24,64 @@ import (
 
 type CircularQueue struct {
 	values []int
-	// need to implement
+	front  int
+	rear   int
 }
 
 func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{} // need to implement
+	return CircularQueue{
+		values: make([]int, size),
+		front:  -1,
+		rear:   -1,
+	}
 }
 
 func (q *CircularQueue) Push(value int) bool {
-	return false // need to implement
+	if q.Full() {
+		return false
+	}
+	if q.front == -1 {
+		q.front = 0
+	}
+	q.rear = (q.rear + 1) % len(q.values)
+	q.values[q.rear] = value
+	return true
 }
 
 func (q *CircularQueue) Pop() bool {
-	return false // need to implement
+	if q.Empty() {
+		return false
+	}
+	if q.front == q.rear {
+		q.front = -1
+		q.rear = -1
+	} else {
+		q.front = (q.front + 1) % len(q.values)
+	}
+
+	return true
 }
 
 func (q *CircularQueue) Front() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+	return q.values[q.front]
 }
 
 func (q *CircularQueue) Back() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+	return q.values[q.rear]
 }
 
 func (q *CircularQueue) Empty() bool {
-	return false // need to implement
+	return q.front == -1 // need to implement
 }
 
 func (q *CircularQueue) Full() bool {
-	return false // need to implement
+	return (q.front == 0 && q.rear == len(q.values)-1) || (q.front == (q.rear+1)%len(q.values))
 }
 
 func TestCircularQueue(t *testing.T) {
