0e37033d9083b74f9f55ae519332d7b4d134daad
Denis Alekseev <d.alekseev@2Gis.ru>
2026-01-28 21:44:01 +0600

    homework-04

--- homework/maps/homewrok_test.go
+++ homework/maps/homewrok_test.go
@@ -7,34 +7,182 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+/*
+type OrderedMap struct { ... }
+
+func NewOrderedMap() OrderedMap                      // создать упорядоченный словарь
+func (m *OrderedMap) Insert(key, value int)          // добавить элемент в словарь
+func (m *OrderedMap) Erase(key int)                  // удалить элемент из словари
+func (m *OrderedMap) Contains(key int) bool          // проверить существование элемента в словаре
+func (m *OrderedMap) Size() int                      // получить количество элементов в словаре
+func (m *OrderedMap) ForEach(action func(int, int))  // применить функцию к каждому элементу словаря от меньшего к большему
+*/
 // go test -v homework_test.go
 
-type OrderedMap struct {
-	// need to implement
+type node struct {
+	key   int
+	value int
+	left  *node
+	right *node
 }
 
+type OrderedMap struct {
+	root *node
+	size int
+}
+
+// NewOrderedMap создает новый упорядоченный словарь
 func NewOrderedMap() OrderedMap {
-	return OrderedMap{} // need to implement
+	return OrderedMap{
+		root: nil,
+		size: 0,
+	}
 }
 
 func (m *OrderedMap) Insert(key, value int) {
-	// need to implement
+	if m.root == nil {
+		m.root = &node{key: key, value: value}
+		m.size++
+		return
+	}
+
+	current := m.root
+	var parent *node
+
+	// Поиск места для вставки или обновления
+	for current != nil {
+		parent = current
+		if key < current.key {
+			current = current.left
+		} else if key > current.key {
+			current = current.right
+		} else {
+			// Ключ уже существует - обновляем значение
+			current.value = value
+			return
+		}
+	}
+
+	// Вставка нового узла
+	newNode := &node{key: key, value: value}
+	if key < parent.key {
+		parent.left = newNode
+	} else {
+		parent.right = newNode
+	}
+	m.size++
+}
+func (m *OrderedMap) findNode(key int) (*node, *node) {
+	var parent *node
+	current := m.root
+
+	for current != nil {
+		if key == current.key {
+			return current, parent
+		}
+
+		parent = current
+		if key < current.key {
+			current = current.left
+		} else {
+			current = current.right
+		}
+	}
+
+	return nil, nil
+}
+
+func minNode(n *node) (*node, *node) {
+	if n == nil {
+		return nil, nil
+	}
+
+	var parent *node
+	current := n
+
+	for current.left != nil {
+		parent = current
+		current = current.left
+	}
+
+	return current, parent
 }
 
 func (m *OrderedMap) Erase(key int) {
-	// need to implement
+	nodeToDelete, parent := m.findNode(key)
+
+	if nodeToDelete == nil {
+		// Элемент не найден
+		return
+	}
+
+	// Случай 1: У узла нет детей или только один ребенок
+	if nodeToDelete.left == nil || nodeToDelete.right == nil {
+		var child *node
+		if nodeToDelete.left != nil {
+			child = nodeToDelete.left
+		} else {
+			child = nodeToDelete.right
+		}
+
+		if parent == nil {
+			// Удаляем корень
+			m.root = child
+		} else {
+			if parent.left == nodeToDelete {
+				parent.left = child
+			} else {
+				parent.right = child
+			}
+		}
+		m.size--
+		return
+	}
+
+	// Случай 2: У узла два ребенка
+	// Находим преемника (минимальный элемент в правом поддереве)
+	successor, successorParent := minNode(nodeToDelete.right)
+
+	// Если преемник не является прямым правым ребенком
+	if successorParent == nil {
+		successorParent = nodeToDelete
+	}
+
+	// Копируем ключ и значение преемника
+	nodeToDelete.key = successor.key
+	nodeToDelete.value = successor.value
+
+	// Удаляем преемника
+	if successorParent.left == successor {
+		successorParent.left = successor.right
+	} else {
+		successorParent.right = successor.right
+	}
+
+	m.size--
 }
 
 func (m *OrderedMap) Contains(key int) bool {
-	return false // need to implement
+	node, _ := m.findNode(key)
+	return node != nil
 }
 
 func (m *OrderedMap) Size() int {
-	return 0 // need to implement
+	return m.size // need to implement
+}
+
+func inOrderTraversal(n *node, action func(int, int)) {
+	if n == nil {
+		return
+	}
+
+	inOrderTraversal(n.left, action)
+	action(n.key, n.value)
+	inOrderTraversal(n.right, action)
 }
 
 func (m *OrderedMap) ForEach(action func(int, int)) {
-	// need to implement
+	inOrderTraversal(m.root, action)
 }
 
 func TestCircularQueue(t *testing.T) {
