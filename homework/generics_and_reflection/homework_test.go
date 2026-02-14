ccd14998397c3ccbd6d43e38b730ed8c71f52f8e
Denis Alekseev <d.alekseev@2Gis.ru>
2026-02-14 13:57:45 +0600

    homework-09

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,8 +20,65 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
-	return ""
+	v := reflect.ValueOf(person)
+	t := reflect.TypeOf(person)
+	res := []string{}
+	num := v.NumField()
+	for i := range num {
+		tag, ok := t.Field(i).Tag.Lookup("properties")
+		if !ok {
+			continue
+		}
+		name := tag
+		oe := false
+		if strings.Contains(tag, "omitempty") {
+			name = strings.Replace(name, ",omitempty", "", 1)
+			oe = true
+		}
+		fmt.Println(name, oe, v.Field(i).IsZero())
+		if oe && v.Field(i).IsZero() {
+			continue
+		}
+
+		if !oe && v.Field(i).IsZero() {
+			res = append(res, name+"="+emptyValue(t.Field(i)))
+			continue
+		}
+
+		res = append(res, name+"="+nonEmptyValue(t.Field(i), v.Field(i)))
+
+		// fmt.Println(v.Field(i))
+		// fmt.Println(tag)
+		// fmt.Println(v.Field(i).Kind().String())
+		// fmt.Println(reflect.Zero(t.Field(i).Type))
+	}
+	return strings.Join(res, "\n")
+}
+
+func emptyValue(t reflect.StructField) string {
+	// рассматриваю не все типы, только те что есть в задании, чисто для ускорения
+	switch t.Type.Kind() {
+	case reflect.Int:
+		return "0"
+	case reflect.Bool:
+		return "false"
+	default:
+		// по хорошему нужны ещё пустые массивы и объекты
+		return ""
+	}
+}
+
+func nonEmptyValue(t reflect.StructField, v reflect.Value) string {
+	switch t.Type.Kind() {
+	case reflect.Int, reflect.Int16, reflect.Int8, reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(int64(v.Int()), 10)
+	case reflect.Bool:
+		return strconv.FormatBool(v.Bool())
+	case reflect.String:
+		return v.String()
+	default:
+		return ""
+	}
 }
 
 func TestSerialization(t *testing.T) {
